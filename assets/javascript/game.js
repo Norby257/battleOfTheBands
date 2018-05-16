b0cd89a5c50be25c7de86a38c7dcb5d06301add6
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-05-16 18:05:19 -0500

    initial JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,17 @@
 console.log("We are linked");
+
+
+$(document).ready(function() {
+    $(".image").on("click", function() {
+        console.log("I've been clicked");
+
+        //  get the exact fighter that was chosen
+
+        var fighter = $(this).attr("fighter");
+        console.log(fighter);
+    })
+
+
+
+
+})
